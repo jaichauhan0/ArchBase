0de43d658a6d3a42d5c05d1f83d62fad45be7e4e
jaichauhan0 <cdhananjay786@gmail.com>
2026-04-08 17:15:36 +0530

    Remove bridge-utils from installation packages

--- ArchBase.sh
+++ ArchBase.sh
@@ -14,7 +14,7 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp qemu edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset gufw sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp qemu edk2-ovmf  dnsmasq vde2 openbsd-netcat iptables-nft ipset gufw sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 pacman -S --noconfirm xf86-video-intel
 # pacman -S --noconfirm xf86-video-amdgpu
