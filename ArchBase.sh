c5c4df344fe15a188866d4f74eadb4edfb68af0a
jaichauhan0 <cdhananjay786@gmail.com>
2026-04-08 17:32:18 +0530

    Update ArchBase.sh

--- ArchBase.sh
+++ ArchBase.sh
@@ -5,7 +5,7 @@ hwclock --systohc
 sed -i 's/^# *en_IN.UTF-8 UTF-8/en_IN.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_IN.UTF-8" >> /etc/locale.conf
-echo "arch" >> /etc/hostname
+echo "ArchLinux" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 ArchLinux.localdomain ArchLinux" >> /etc/hosts
